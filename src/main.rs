998a9479803365d2c8c44558f9dd9e512334ec5a
Alen Šiljak <dev@alensiljak.eu.org>
2022-08-15 09:59:15 +0200

    example with logging

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,31 @@
 use axum::{
-    routing::get,
+    routing::{get,post},
     Router,
 };
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    //println!("Hello, world!");
+
+    // tracing init
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::EnvFilter::new(
+            std::env::var("RUST_LOG").unwrap_or_else(|_| "cashier-server=debug".into()),
+        ))
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    // build our application with a single route
+    let app = Router::new()
+        .route("/", get(|| async { "Hello, World!" }));
+
+    let address = std::net::SocketAddr::from(([0,0,0,0], 3000));
+    tracing::debug!("listening on {}", address);
+
+    // run it with hyper on localhost:3000
+    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
