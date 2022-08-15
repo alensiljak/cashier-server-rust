a4d9770f4aed090b47bfee2df316b64f07d04af3
Alen Šiljak <dev@alensiljak.eu.org>
2022-08-15 10:45:07 +0200

    method for ledger commands

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 use axum::{
     routing::{get,post},
-    Router,
+    response::IntoResponse,
+    Json, Router,
 };
-use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+use serde::{Deserialize, Serialize};
+use std::net::SocketAddr;
+//use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 use tower_http::cors::{Any, CorsLayer};
 
 #[tokio::main]
@@ -10,27 +13,55 @@ async fn main() {
     //println!("Hello, world!");
 
     // tracing init
-    tracing_subscriber::registry()
-        .with(tracing_subscriber::EnvFilter::new(
-            std::env::var("RUST_LOG").unwrap_or_else(|_| "cashier-server=debug".into()),
-        ))
-        .with(tracing_subscriber::fmt::layer())
-        .init();
+    // tracing_subscriber::registry()
+    //     .with(tracing_subscriber::EnvFilter::new(
+    //         std::env::var("RUST_LOG").unwrap_or_else(|_| "cashier-server=debug".into()),
+    //     ))
+    //     .with(tracing_subscriber::fmt::layer())
+    //     .init();
+    tracing_subscriber::fmt::init();
 
     let cors = CorsLayer::new().allow_origin(Any);
 
     // build our application with a single route
     let app = Router::new()
-        .route("/", get(|| async { "Hello, World!" }))
+        //.route("/", get(|| async { "Hello, World!" }))
+        .route("/", get(root))
+        .route("/ledger", get(ledger))
+        .route("/ping", get(|| async { "pong" }))
         .layer(cors);
 
-    let address = std::net::SocketAddr::from(([0,0,0,0], 3000));
+    let address = SocketAddr::from(([0,0,0,0], 3000));
     tracing::debug!("listening on {}", address);
 
     // run it with hyper on localhost:3000
-    // &"0.0.0.0:3000".parse().unwrap()
     axum::Server::bind(&address)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+// basic handler that responds with a static string
+async fn root() -> &'static str {
+    "Hello, World!"
+}
+
+async fn ledger(
+    // this argument tells axum to parse the request body
+    // as JSON into a `CreateUser` type
+    Json(payload): Json<LedgerCommand>
+) -> &'static str {
+    //todo: run ledger
+
+    "Ledger endpoint. You asked for {}", payload
+}
+
+#[derive(Deserialize)]
+struct LedgerCommand {
+    command: String,
+}
+
+#[derive(Serialize)]
+struct LedgerOutput {
+    output: String,
 }
