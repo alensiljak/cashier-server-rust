6daaaa9f1c3e918dcbc4a95333806df4aaa3ff5d
Alen Šiljak <dev@alensiljak.eu.org>
2023-03-03 23:35:15 +0100

    the lib is too simple for real interaction

--- tests/repl.rs
+++ tests/repl.rs
@@ -3,22 +3,37 @@ use std::{process::Command, thread::sleep, time::Duration};
 /**
  * Testing running Ledger in REPL mode.
  */
+
+/*
 use interactive_process::InteractiveProcess;
 
 #[test]
 fn run_repl() {
-    let cmd = Command::new("ledger");
+    let mut cmd = Command::new("ledger");
+    cmd.args(["-f", "tests/journal.ledger"]);
 
     let mut proc = InteractiveProcess::new(cmd, |line| {
-        println!("Got: {}", line.unwrap());
+        let text = line.unwrap();
+        if !text.starts_with(']') {
+            return;
+        }
+
+        println!("Got: {}", text);
     })
     .unwrap();
+    // sleep(Duration::from_secs(1));
 
-    proc.send("b master").unwrap();
+    proc.send("b cash").unwrap();
     sleep(Duration::from_secs(1));
-    
-    sleep(Duration::from_millis(1));
-    proc.close().kill().unwrap();
-    
-    assert!(false);
+
+    // sleep(Duration::from_millis(1));
+    // proc.close().kill().unwrap();
+
+    // Close by issuing a command.
+    proc.send("quit").unwrap();
+    proc.wait().unwrap();
+    //sleep(Duration::from_millis(1));
+
+    assert!(true);
 }
+*/
